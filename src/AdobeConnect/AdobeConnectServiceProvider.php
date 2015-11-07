4e6a9e979e62a6e67bcdc7c1c25215ee0c733bc0
Jorge Sanchez <jorge@parachute.pe>
2015-11-07 12:41:49 -0500

    Creating a ServiceProvider

---
+++ src/AdobeConnect/AdobeConnectServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AdobeConnect;
+
+use Illuminate\Support\ServiceProvider;
+
+class AdobeConnectServiceProvider extends ServiceProvider {
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register() {
+        $this->app->singleton('Adobe\Contracts\Connection', function ($app) {
+            $config = $app['config'];
+            $host = $config->get('adobe.host');
+            $username = $config->get('adobe.username');
+            $password = $config->get('adobe.password');
+
+            $config = new Config($host, $username, $password);
+            return new Connection($config);
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides(){
+        return array('adobeconnect');
+    }
+
+}
